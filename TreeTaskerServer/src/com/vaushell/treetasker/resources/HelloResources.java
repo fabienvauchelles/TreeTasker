6b0c752ec78b5249b39ab8d384828d0b39ea238b
Fabien VAUCHELLES <fabien@vaushell.com>
2012-06-13 19:15:40 +0200

    ajout des répertoires projets serveur et android

---
+++ TreeTaskerServer/src/com/vaushell/treetasker/resources/HelloResources.java
@@ -0,0 +1,18 @@
+package com.vaushell.treetasker.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path( "/hello" )
+public class HelloResources
+{
+	// PUBLIC
+	@GET
+	@Produces( MediaType.TEXT_PLAIN )
+	public String getStandard()
+	{
+		return "Hello world";
+	}
+}
