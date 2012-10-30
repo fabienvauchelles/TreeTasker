a5b5cc1138d086300c45e0238dbe3fe757cdaf96
Frederic PEAK <fred@vaushell.com>
2012-10-30 12:04:35 +0100

    * Modification du nom de l'application (closes #315)
    * Ajout de trim pour les valeurs (closes #314)
    * Utilisation de la bonne activité au lancement.

--- TreeTaskerAD/src/com/vaushell/treetasker/application/activity/TT_EditTaskActivity.java
+++ TreeTaskerAD/src/com/vaushell/treetasker/application/activity/TT_EditTaskActivity.java
@@ -17,12 +17,18 @@ import com.vaushell.treetasker.R;
 import com.vaushell.treetasker.model.TT_Task;
 
 public class TT_EditTaskActivity
-    extends FragmentActivity
+	extends FragmentActivity
 {
+	public void onCancel(
+		View view ) {
+		setResult( RESULT_CANCELED );
+		finish();
+	}
+
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate( Bundle savedInstanceState )
-	{
+	public void onCreate(
+		Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.edit_task_activity_view );
 		task = (TT_Task) getIntent().getExtras().getSerializable( "task" );
@@ -30,23 +36,15 @@ public class TT_EditTaskActivity
 		( (EditText) findViewById( R.id.aTFdescriptionValue ) ).setText( task.getDescription() );
 	}
 
-	public void onSave( View view )
-	{
-		task.setTitle( ( (EditText) findViewById( R.id.aTFtitleValue ) ).getText()
-		                                                                .toString() );
-		task.setDescription( ( (EditText) findViewById( R.id.aTFdescriptionValue ) ).getText()
-		                                                                            .toString() );
+	public void onSave(
+		View view ) {
+		task.setTitle( ( (EditText) findViewById( R.id.aTFtitleValue ) ).getText().toString().trim() );
+		task.setDescription( ( (EditText) findViewById( R.id.aTFdescriptionValue ) ).getText().toString().trim() );
 		Intent resultData = new Intent();
 		Bundle resultBundle = new Bundle();
 		resultBundle.putSerializable( "task", task );
 		resultData.putExtras( resultBundle );
 		setResult( RESULT_OK, resultData );
-		finish();
-	}
-
-	public void onCancel( View view )
-	{
-		setResult( RESULT_CANCELED );
 		finish();
 	}
 
